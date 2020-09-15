6ed5d968fdcdd985073a66593a96320bc1f06af1
Josh Barnes <JBarnes49@gmail.com>
2020-09-15 14:18:35 -0700

    added cors functionality and security

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const MOVIEDEX = require('./movies-data-small.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
