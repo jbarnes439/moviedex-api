9330db2309f52da162c05c76963985c081faa495
Josh Barnes <JBarnes49@gmail.com>
2020-09-15 14:03:38 -0700

    filters fully functional!

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const MOVIEDEX = require('./movies-data-small.json');
 
 const app = express();
 
 app.use(morgan('dev'));
 
-const validMovies = ['Rambo', 'Die Hard', 'Die Hard 2', 'Puss-n-Boots'];
+function validateBearerToken(req, res, next) {
+  const authToken = req.get('Authorization');
+  const apiToken = process.env.API_TOKEN;
 
-function handleGetMovies(req, res) {
-  res.json(validMovies);
+  console.log('validate bearer token middleware');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
 }
 
-app.get('/movies', handleGetMovies);
+function handleGetMovies(req, res) {
+  let response = MOVIEDEX;
+
+  // filter query by genre if genre query present
+  if (req.query.genre) {
+    response = response.filter(movie => {
+      return movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
+    });    
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie => {
+      return movie.country.toLowerCase().includes(req.query.country.toLowerCase());
+    });
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(movie => {
+      return movie.avg_vote >= parseInt(req.query.avg_vote);
+    });
+  }
+
+  res.json(response);
+}
+
+app.get('/movies', validateBearerToken, handleGetMovies);
 
 const PORT = 5050;
 
