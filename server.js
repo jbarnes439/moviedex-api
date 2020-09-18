3f497de3a1d192e4e4a5d0ee7b28d19e0a241e31
Josh Barnes <JBarnes49@gmail.com>
2020-09-18 13:39:35 -0700

    prepared movie-dex for deployment to heroku

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const MOVIEDEX = require('./movies-data-small.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
-  const apiToken = process.env.API_TOKEN;
-
-  console.log('validate bearer token middleware');
+  const apiToken = process.env.API_TOKEN;  
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
@@ -50,7 +49,17 @@ function handleGetMovies(req, res) {
 
 app.get('/movies', validateBearerToken, handleGetMovies);
 
-const PORT = 5050;
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: {message: 'server error'}}
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
